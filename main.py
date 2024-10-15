647ff0f4f5783ba978d86de67a59436d0fb0bb97
Gabriel Palma <144812765+GabrielP012@users.noreply.github.com>
2024-10-15 18:56:57 -0400

    Add files via upload
    Adding the input selection.

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ with AnimeFLV() as api:
   elements = api.search(input('Write the anime name: '))
   for i, element in enumerate(elements):
     print(f'{i}, {element.title}')
-    
+  try:
+    input_selection = (input('select option: '))
+    while not input_selection.isdigit():
+      input_selection = (input('Invalid Number.  Select option: '))
+    selection = int(input_selection)
+    info = api.get_anime_info(elements[selection].id)
+    info.episodes.reverse()
+  except Exception as e:
+    print(e)
