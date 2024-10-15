be9c83bcf582773cabeb783d875880e49afb7d26
Gabriel Palma <144812765+GabrielP012@users.noreply.github.com>
2024-10-15 18:22:10 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from animeflv import AnimeFLV
 with AnimeFLV() as api:
   elements = api.search(input('Write the anime name: '))
+  for i, element in enumerate(elements):
+    print(f'{i}, {element.title}')
+    
